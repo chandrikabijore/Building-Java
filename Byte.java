4e26cf7f866b6acbbd0c5d9332658196ecbd77a6
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-19 19:30:25 +0530

    Add files via upload

---
+++ Byte.java
@@ -0,0 +1,15 @@
+package com.chandrika;
+
+public class Byte
+{
+    public static void main(String[] args) {
+        //byte b = 10;
+        //byte b = 128;
+        //byte b = 10.9;
+        //byte b = true;
+        //byte b = "shivi";
+        byte b = 127;
+
+        System.out.println(b);
+    }
+}
---
+++ Short.java
@@ -0,0 +1,12 @@
+package com.chandrika;
+
+public class Short 
+{
+    public static void main(String[] args)
+    {
+        short a = 32767;
+        //short a = 32768;
+        //short a = "shivi";
+        System.out.println(a);
+    }
+}
