58651e344c4f8d8cca58615f64c778611c6660a2
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-19 19:30:47 +0530

    Add files via upload

---
+++ Data_Type.java
@@ -0,0 +1,35 @@
+package com.chandrika;
+
+public class Data_Type
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\nfor byte Data type : ");
+        System.out.println("The min value of byte is : " + - 128);
+        System.out.println("The max value of byte is : " + 127);//
+
+        System.out.println("\nfor short Data type : ");
+        System.out.println("The min value of short is : " + -32767);
+        System.out.println("The max value of byte is : " + 32767);
+
+        System.out.println("\nfor int Data type : ");
+        System.out.println("The min value of int is : " + -2147483647);
+        System.out.println("The max value of byte is : " + 2147483647);
+
+//        System.out.println("\nfor long Data type : ");
+//        System.out.println("\nThe min value of long is : " + - 9223372036854775808);
+//        System.out.println("\nThe max value of long is : " + 9223372036854775807);
+
+        System.out.println("\nfor float Data type : ");
+        System.out.println("The min value of float is : " + - 3.4e38);
+        System.out.println("The max value of float is : " + 3.4e38);
+
+        System.out.println("\nfor double Data type : ");
+        System.out.println("The min value of double is : " + - 1.7e38);
+        System.out.println("The max value of double is : " + 1.7e38);
+
+        System.out.println("\nfor char Data type : ");
+        System.out.println("The min value of char is : " + ('\u0000'));
+        System.out.println("The max value of char is : " + 65535);
+    }
+}
