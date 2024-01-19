772b5842265d89e138dd146c939cad5209723a7d
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-19 18:27:47 +0530

    Add files via upload

---
+++ Alpha_presnims.java
@@ -0,0 +1,15 @@
+// All predeined and Alpha_presnims we can use as identifier
+
+package com.chandrika;
+
+public class Alpha_presnims
+{
+    public static void main(String[] args)
+    {
+        int string = 888;
+        System.out.println(string);
+
+        int Runnable = 999;
+        System.out.println(Runnable);
+    }
+}
