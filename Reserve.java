1628a211f491bd4fb1d484758b5e23404137bf8f
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-19 18:27:25 +0530

    Add files via upload

---
+++ Reserve.java
@@ -0,0 +1,14 @@
+// we can not use Reserve words as Identifier.
+
+package com.chandrika;
+
+public class Reserve
+{
+    public static void main(String[] args)
+    {
+        int x = 100;
+    //  int if = 200;
+        System.out.println(x);
+    //  System.out.println(if);
+    }
+}
