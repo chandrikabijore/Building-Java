f61c8439b83e378e85e6439d6e01dee9ea11eb52
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-19 18:27:01 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,14 @@
+package com.chandrika;
+
+public class Number
+{
+    public static void main(String[] args) {
+        int NUMBER = 10;
+        int number = 20;
+        int Number = 30;
+
+        System.out.println(NUMBER);
+        System.out.println(number);
+        System.out.println(Number);
+    }
+}
