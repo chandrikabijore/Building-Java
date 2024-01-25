fdb9a8d5fa257eb2bc35ea11a45b1cc408fe9fba
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-26 05:18:53 +0530

    Array

---
+++ 0014 - Array_Intro.java
@@ -0,0 +1,37 @@
+package com.chandrika;
+
+public class Array_Intro
+{
+    public static void main(String[] args)
+    {
+        // one dimentinal Array Declaration
+        int[] x;
+        int []y;
+        int z[];
+
+        // Two dimentinal Array Declaration
+        //int[][]    x;
+        //int        [][]x;
+        //int        []z[];
+        //int[]      a[];
+        //int[]      []a;
+
+        // Three dimentinal Array Declaration
+        //int[][][]    a;
+        //int        [][][]a;
+       //int          a[][][];
+        //int[]      a[][];
+        //int[]      [][]a;
+        //int[]      []a[];
+        //int[][]      a[];
+        //int[][]      []a;
+        //int          [][]a[];
+        //int[][]      []a[][];
+
+//        main(String[] args)
+//        {
+//            System.out.println(args.length);
+//        }
+        
+    }
+}
