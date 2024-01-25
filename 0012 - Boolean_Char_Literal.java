d3bc9de26dc3c3cdd24d5c7c33b24a677a634522
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-25 18:38:14 +0530

    Literal

---
+++ 0012 - Boolean_Char_Literal.java
@@ -0,0 +1,33 @@
+package com.chandrika;
+
+public class Boolean_Char_Literal
+{
+    public static void main(String[] args)
+    {
+        boolean b = true;
+       // boolean b = 0;
+       // boolean b = True;
+       // boolean b = "True";
+
+        System.out.println(b);
+
+       // char ch = 'a';
+       // char ch = 'ab';
+       // char ch = a;
+       // char ch = "chand";
+       // char ch1 = OXFace;
+        char c = 65535;
+        char ch = '\u0061';
+
+        System.out.println(c);
+        System.out.println(ch);
+
+       char ch4 = '\ubeaf';
+       // char ch4 = '\m';
+       // char ch4 = '\face';
+
+       System.out.println(ch4);
+
+    }
+
+}
