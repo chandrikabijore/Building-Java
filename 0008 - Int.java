15fe8aa12d3852ce04ccd2ea66a9edf9c2b491da
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-25 07:57:06 +0530

    Int.java

--- 0008 - Int.java
+++ 0008 - Int.java
@@ -4,7 +4,9 @@ public class Int
 {
     public static void main(String[] args) {
         int x = 2147483647;
+        int x1 = - 2147483648;
         System.out.println("This is the max value of int : " +x);
+        System.out.println("This is the min value of int : " +x);
 
         //int y = 2147483648;
         //int y = 2147483648l;
