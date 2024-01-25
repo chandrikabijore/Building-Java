84a4e5636d6239984f08a774fcf563f885cc9656
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-25 18:00:33 +0530

    Literal

---
+++ 0011 - FloatingPoint_Literal.java
@@ -0,0 +1,20 @@
+package com.chandrika;
+
+public class FloatingPoint_Literal
+{
+    public static void main(String[] args)
+    {
+        //float F = 123.456;
+        float F = 123.456F;
+        //double d = 123.456;
+        double d1 = 123.456D;
+        //  float d1 = 123.456d;
+        // double d1 = 0X123.456;
+
+        //  double d = 0786;
+        //  double o = OXface;
+        double d = 0786.0;
+
+        System.out.println(F + "..." + d + "..." + d1 + "..." );
+    }
+}
