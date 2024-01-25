3042a80e287301dff78e9e352ffed2bd03d9c9b8
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-25 22:01:35 +0530

    Literals

---
+++ 1.7_Version_Enhancement.java
@@ -0,0 +1,15 @@
+package com.chandrika;
+
+public class Version_Enhancement_
+{
+    public static void main(String[] args)
+    {
+       // int x = 0B111;              // it is not supported currently in java
+       // System.out.println(x);
+
+       // double d = 123456.789;
+       // double d1 = 12_34_56.78_9;
+
+      // System.out.println(d);
+    }
+}
