4d12a48b13c5fca0b7d22c6cbc535e164f774cf6
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-27 05:14:48 +0530

    Array

---
+++ 0022 - Array_using_standard_library.java
@@ -0,0 +1,16 @@
+package com.chandrika;
+import java.util.Arrays;
+
+public class Array_using_standard_library
+{
+    public static void main(String[] args)
+    {
+        // toString() is the built-in method of java.lang which return itself a string. 
+        // So here no actual conversion is performed. Since toString() method simply 
+        // returns the current string without any changes, there is no need to call 
+        // the string explicitly, it is usually called implicitly.
+        
+        int[] array = {1, 3, 5, 7, 9, 11, 13, 15 , 17, 19, 21};
+        System.out.println(Arrays.toString(array));        
+    }
+} 
