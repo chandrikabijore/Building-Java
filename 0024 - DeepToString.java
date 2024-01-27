2c32c95d2c6331be8dd051f73fb9ca90feb11d0b
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-27 05:54:33 +0530

    Array

---
+++ 0024 - DeepToString.java
@@ -0,0 +1,12 @@
+package com.chandrika;
+
+import java.util.Arrays;
+
+public class DeepToString
+{
+    public static void main(String[] args)
+    {
+        int[][] arr = {{10, 20}, {30, 40, 50}, {60, 70}};
+        System.out.println(Arrays.deepToString(arr));
+    }
+}
