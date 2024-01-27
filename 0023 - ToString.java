8a5ac75191ec636d22f84d39bc6c9fb210cb2073
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-27 05:53:38 +0530

    Array

---
+++ 0023 - ToString.java
@@ -0,0 +1,20 @@
+// java program to illustrat the
+// java.lang.String.toString() method
+
+package com.chandrika;
+import java.io.*;
+
+public class ToString
+{
+    public static void main(String[] args)
+    {
+        String Strobj = new String("Welcome to the world of Shivi....!!");
+        System.out.print("Output String Value :");
+        System.out.println(Strobj.toString());
+
+        String Strobj2 = new String("Let's make it simple for you.");
+        System.out.print("Output String Value :");
+        System.out.println(Strobj2.toString());
+    }
+
+}
