fcda6950cb253f34446fb0df74507947cb231df2
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-01-28 18:37:46 +0530

    Variable

---
+++ 0032 - Variable_Local4.java
@@ -0,0 +1,12 @@
+package com.chandrika;
+
+public class Variable_Local4
+{
+    int x = 10;             // instance variable
+    static int y = 20;      // static variable
+
+    public static void main(String[] args)
+    {
+        int z = 30;        // local variable
+    }
+}
