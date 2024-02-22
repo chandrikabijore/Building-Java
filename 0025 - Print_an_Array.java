1b6bd93d0d4a2589b17c14f135f424a0de5c855a
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-02-22 12:42:26 +0530

    Array

---
+++ 0025 - Print_an_Array.java
@@ -0,0 +1,13 @@
+package com.chandrika;
+
+public class Print_an_Array
+{
+    public static void main(String[] args) {
+        int[] Array = {1, 2, 3, 4, 5};
+
+        for (int element : Array)
+        {
+            System.out.println(element);
+        }
+    }
+}
