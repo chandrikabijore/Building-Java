097befb25792dfd8f596573c1cc0b6b135dec923
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-04 12:57:59 +0530

    Array_1.java

---
+++ 25 - Array_1.java
@@ -0,0 +1,14 @@
+package com.chandrika;
+
+public class Array_1
+{
+    public static void main(String[] args)
+    {
+        int arr[] = {10, 20, 30, 40, 50};
+        System.out.println(arr.length);
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.println("Printing array elements : " + arr[i] + " ");
+        }
+    }
+}
