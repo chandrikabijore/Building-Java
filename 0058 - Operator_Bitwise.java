9e1ca1cfa8528b6e00e22c3a505075d4ae7bd391
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-04 13:29:56 +0530

    Operator

---
+++ 0058 - Operator_Bitwise.java
@@ -0,0 +1,17 @@
+package com.chandrika;
+
+public class Operator_Bitwise
+{
+    public static void main(String[] args)
+    {
+        System.out.println(true & false);            // false
+        System.out.println(true | false);            // true
+        System.out.println(true ^ false);            // true
+
+        System.out.println(4 & 5);                   // 4
+        System.out.println(4 | 5);                   // 5
+        System.out.println(4 ^ 5);                   // 1
+    }
+}
+
+
