e3676771b2aa1696eb2f23a746e48b50637e313d
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-04 13:32:08 +0530

    Operator

---
+++ 0060 - Operator_BooleanComplement.java
@@ -0,0 +1,9 @@
+package com.chandrika;
+
+public class Operator_BooleanComplement
+{
+    public static void main(String[] args) {
+        //System.out.println(!4);
+        System.out.println(!false);
+    }
+}
