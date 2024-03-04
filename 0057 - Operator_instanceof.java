150902b1136635ecf469b3b16e9e294034c68431
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-04 13:27:55 +0530

    Create 0057 - Operator_instanceof

---
+++ 0057 - Operator_instanceof.java
@@ -0,0 +1,22 @@
+package com.chandrika;
+
+public class Operator_instanceof
+{
+    public static void main(String[] args)
+    {
+        Thread t = new Thread();
+        // EXAMPLE - 1
+        System.out.println(t instanceof Thread);
+        System.out.println(t instanceof Object);
+        System.out.println(t instanceof Runnable);
+
+        // EXAMPLE - 2
+        //System.out.println(t instanceof String);
+
+        // EXAMPLE - 3
+        System.out.println(null instanceof Thread);
+        System.out.println(null instanceof Object);
+        System.out.println(null instanceof Runnable);
+    }
+}
+          
