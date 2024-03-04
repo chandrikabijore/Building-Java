926ec6aacdbf4403b918da29fd060aa49b75bba6
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-04 13:30:43 +0530

    Operator_BitwiseComplement

---
+++ 0059 - Operator_BitwiseComplement.java
@@ -0,0 +1,10 @@
+package com.chandrika;
+
+public class Operator_BitwiseComplement
+{
+    public static void main(String[] args)
+    {
+        //System.out.println(~true);
+        System.out.println(~4);
+    }
+}
