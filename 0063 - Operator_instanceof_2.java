7f28a1f245ee9439694ace26f1c0aa586e6a9440
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-05 16:47:17 +0530

    Operator

---
+++ 0063 - Operator_instanceof_2.java
@@ -0,0 +1,14 @@
+package com.chandrika;
+
+public class Operator_instanceof_2
+{
+}
+class Dog extends Operator_instanceof_2
+{
+    public static void main(String[] args)
+    {
+        Dog d = new Dog();
+        System.out.println(d instanceof Operator_instanceof_2);
+    }
+}
+
