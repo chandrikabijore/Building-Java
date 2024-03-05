df4743ff66b8e334fa20a47d2ca1a9c7463dc96d
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-05 17:02:43 +0530

    Operator

---
+++ 0067 - Operator_Assignment.java
@@ -0,0 +1,12 @@
+package com.chandrika;
+
+public class Operator_Assignment
+{
+    public static void main(String[] args)
+    {
+        int b, c, d;
+        // int a = b = c = d;
+
+        int a1 = 20;
+    }
+}
