ac77576ef19e44ec3cfa1f853713b0d7a41d3b1a
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-05 16:46:20 +0530

    Operator

---
+++ 0062 - Operator_instanceof_1.java
@@ -0,0 +1,12 @@
+package com.chandrika;
+
+public class Operator_instanceof_1
+{
+    public static void main(String[] args)
+    {
+        Operator_instanceof_1 o = new Operator_instanceof_1();
+        System.out.println(o instanceof Operator_instanceof_1);
+
+
+    }
+}
