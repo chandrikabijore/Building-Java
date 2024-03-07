d71249f0a393237dc30cd461124aae3eb4a70aaa
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-07 22:24:28 +0530

    Flow_Control

---
+++ 0076 - Flow_Control_switch_2.java
@@ -0,0 +1,24 @@
+package com.chandrika;
+
+public class Flow_Control_switch_2
+{
+    public static void main(String[] args)
+    {
+        //1. curly braces are mandatory
+        // switch ()
+
+        //2. empty switch is valid
+        int x = 10;
+        switch (x)
+        {
+        }
+
+        //3. case & default is optinal
+        int x1 = 100;
+        switch (x1)
+        {
+           // System.out.println("Invalid");
+        }
+    }
+}
+
