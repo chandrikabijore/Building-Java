74c1a4506334a1fccb30b593c4f87feaaec4f826
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-07 14:20:19 +0530

    Create 0072 - instanceof_Vs_isInstanceMethod.java

---
+++ 0072 - instanceof_Vs_isInstanceMethod.java
@@ -0,0 +1,10 @@
+package com.chandrika;
+
+public class instanceof_Vs_isInstanceMethod
+{
+    public static void main(String[] args) //throws exception
+    {
+//        Thead t = new Thead();
+//        System.out.println(Class.forName(args[0]),isInstanceof(t));
+    }
+}
