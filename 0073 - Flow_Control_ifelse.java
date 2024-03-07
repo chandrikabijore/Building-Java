6e5c2a6984eacb2be9aab7863a0b2ac2ada735ea
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-07 14:34:53 +0530

    Flow_Control

---
+++ 0073 - Flow_Control_ifelse.java
@@ -0,0 +1,76 @@
+package com.chandrika;
+
+public class Flow_Control_ifelse
+{
+    public static void main(String[] args) {
+//        int x = 0;
+//        if(x)
+//        {
+//            System.out.println("Hello");
+//        }
+//        else
+//        {
+//            System.out.println("Hiii");
+//        }
+
+//        int x = 10;
+//        if(20)
+//        {
+//            System.out.println("Hello");
+//        }
+//        else
+//        {
+//            System.out.println("Hiii");
+//        }
+
+//        int x = 10;
+//        if (x == 20)
+//        {
+//            System.out.println("Hello");
+//        }
+//        else
+//        {
+//            System.out.println("Hiii");
+//        }
+
+//        boolean b = true;
+//        if (b == false)
+//        {
+//            System.out.println("Hello");
+//        }
+//        else
+//        {
+//            System.out.println("Hiii");
+//        }
+
+        boolean b = false;
+        if (b == false)
+        {
+            System.out.println("Hello");
+        }
+        else
+        {
+            System.out.println("hiiiiii");
+        }
+
+
+        if (true)
+            System.out.println("HELLO");
+
+        if (false);
+
+       // if (true)
+           // int x = 10;
+
+        if (true)
+        {
+            int x = 10;
+        }
+
+        if(true)
+            if(true)
+                System.out.println("HEYY");
+        else
+                System.out.println("hii");
+    }
+}
