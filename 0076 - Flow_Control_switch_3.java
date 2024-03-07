625aceba8a93174b8407af0c512d6469cecc9afa
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-07 22:25:09 +0530

    Flow_Control

---
+++ 0076 - Flow_Control_switch_3.java
@@ -0,0 +1,74 @@
+package com.chandrika;
+
+import java.util.Scanner;
+
+public class Flow_Control_switch_3
+{
+    public static void main(String[] args)
+    {
+//1.      int x = 10;
+//        final int y = 20;
+//        switch (x)
+//        {
+//            case 10:
+//                System.out.println(10);
+//
+//            case y:
+//                System.out.println(20);
+//        }
+
+
+//2.      Scanner SC = new Scanner(System.in);
+//        System.out.println("Enter any number : ");
+//        int x = SC.nextInt();
+//
+//        switch (x+1)
+//        {
+//            case 10:
+//                System.out.println(10);
+//                break;
+//
+//            case 10+20+30:
+//                System.out.println(60);
+//        }
+
+// 3.     byte b = 10;
+//        switch (b)
+//        {
+//            case 10:
+//                System.out.println(10);
+//            case 100:
+//                System.out.println(100);
+//            // case 1000:
+//                System.out.println(1000);
+//        }
+
+//4.      byte b1 = 20;
+//        switch(b1 + 1)
+//        {
+//            case 20:
+//                System.out.println(10);
+//            case 200:
+//                System.out.println(100);
+//            case 2000:
+//                System.out.println(1000);
+//        }
+
+// 5.     int x = 10;
+//        switch (x)
+//        {
+//            case 97:
+//                System.out.println(97);
+//            case 98:
+//                System.out.println(98);
+//            case 99:
+//                System.out.println(99);
+//          //  case 'a':
+//                System.out.println('a');
+//        }
+
+
+    }
+}
+
+
