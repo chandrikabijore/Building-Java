6f208c68c6628badf1c6ce4d2b402e027e0eac2c
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-07 14:19:06 +0530

    Operator

---
+++ 0071 - new_Vs_newInstanceMethod.java
@@ -0,0 +1,25 @@
+package com.chandrika;
+//
+//// import java.util.function.ObjLongConsumer;
+//
+//class Student
+//{
+//}
+//class Customer
+//{
+//}
+//class new_Vs_newInstanceMethod
+//{
+//    public static void main(String[] args) {
+//        Object o = Class.forName(args[0]).newInstance();
+//        System.out.println("Object created for : " + o.getClass().getName());
+//    }
+//}
+// This code won't be run here
+class new_Vs_newInstanceMethod
+{
+    public static void main(String[] args) {
+//        Object o = Class.forName(args[0]).newInstance();
+//        System.out.println("Object created for : " + o.getClass().getName());
+    }
+}
