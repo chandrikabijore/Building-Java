87991db14b29ec2fdb722c083b1c1e1dad24ef23
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-10 10:38:59 +0530

    Iterative_Statement

---
+++ 0081 - Iterative_Statement_do_while.java
@@ -0,0 +1,41 @@
+package com.chandrika;
+
+public class Iterative_Statement_do_while
+{
+    public static void main(String[] args)
+    {
+//        do
+//            System.out.println("Hello");
+//        while (true);
+
+//        do;
+//        while (true);
+
+//        do
+//            int x = 10;                 // CE - declaration not allowed
+//        while (true);
+
+//        do {
+//            int x = 10;
+//            System.out.println("Hello");
+//        }
+//        while (true);
+        
+//        do
+//        while(true);   // CE
+
+
+//        do while(true)
+//            System.out.println("Hello");
+//        while (false);
+
+
+        do
+            while(true)
+                System.out.println("Hello");
+            while (false);
+    }
+
+
+
+}
