db7dd0822bd51703465286c14bb1fc79e19a01ab
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-10 10:37:24 +0530

    Iterative_Statement

---
+++ 0079 - Iterative_Statement_while.java
@@ -0,0 +1,64 @@
+package com.chandrika;
+
+public class Iterative_Statement_while
+{
+    public static void main(String[] args)
+    {
+//        while (1)
+//        {
+//            System.out.println("Hello");
+//        }
+
+//        while(true)
+//            System.out.println("Hello");
+
+
+//        while (true);
+
+
+//        while (true)
+//            int x = 10;
+
+//        while (true)
+//        {
+//            int x = 10;
+//        }
+
+//        while (true)
+//        {
+//            System.out.println("Hello");
+//        }
+//        System.out.println("Hi");
+
+//        while (false)
+//        {
+//            System.out.println("Hello");
+//        }
+//        System.out.println("Hii");
+
+        int a = 10, b = 20;
+        while (a > b)       // try with ( < ) this operator also
+        {
+            System.out.println("Hello");
+        }
+        System.out.println("Hii");
+
+        final int a1 = 10, b1 = 20;
+        while (a1 < b1)       // try with ( > ) this operator also
+        {
+            System.out.println("Hello");
+        }
+        // System.out.println("Hii");
+
+
+//        final int x = 10;
+//        int y = 20;
+//        System.out.println(x);
+//        System.out.println(y);
+    }
+}
+
+
+
+
+
