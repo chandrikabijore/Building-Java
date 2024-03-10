babba22ed5afd6254a3a4c541afc3ee352dd2a74
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-10 10:38:16 +0530

    Iterative_Statement

---
+++ 0080 - Iterative_Statement_while_1.java
@@ -0,0 +1,19 @@
+package com.chandrika;
+
+public class Iterative_Statement_while_1
+{
+    public static void main(String[] args) {
+//        final int a = 10;
+//        int b = 20;
+//        System.out.println(a);
+//        System.out.println(b);
+
+        final int a = 10, b = 20;
+        int c = 20;
+        System.out.println(a+b);
+        System.out.println(a+c);
+        System.out.println(a<b);
+        System.out.println(a<c);
+    }
+}
+
