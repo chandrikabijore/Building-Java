be4a632117704ea738e23196ed7ff3dc34dccc94
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-10 19:53:50 +0530

    Iterative_Statement

---
+++ 0083 - Iterative_Statement_for.java
@@ -0,0 +1,24 @@
+package com.chandrika;
+
+public class Iterative_Statement_for
+{
+    public static void main(String[] args)
+    {
+//        for (int i = 0; true; i++)
+//        {
+//            System.out.println("Hello");
+//        }
+
+//        for (int i = 0; i < 0; i++)
+//        {
+//            System.out.println("Hello");
+//        }
+//        System.out.println("Hii");
+
+//        for (int i = 0; i < 0; i++)
+//            System.out.println("Hello");
+
+//        for (int i = 0; i < 0; i++)
+//            int x = 10;
+    }
+}
