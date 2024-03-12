33e322285ada3c8b30ca0892cedfbcc30c11592b
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-12 08:55:57 +0530

    Create 0087 - D_AM_JavaSourceFileStructure_1.java

---
+++ 0087 - D_AM_JavaSourceFileStructure_1.java
@@ -0,0 +1,35 @@
+package com.chandrika;
+
+public class D_AM_JavaSourceFileStructure_1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("D_AM");
+
+    }
+}
+
+class A
+{
+    public static void main(String[] args) {
+        System.out.println("A class main");
+    }
+}
+
+class B
+{
+    public static void main(String[] args) {
+        System.out.println("B class main");
+    }
+}
+
+class C
+{
+    public static void main(String[] args) {
+        System.out.println("C class main");
+    }
+}
+
+//class D
+//{
+//}
