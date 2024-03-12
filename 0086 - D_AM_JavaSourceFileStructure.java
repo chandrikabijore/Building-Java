7ae7bc1e01864dd386987eb9237a6b0c96ad6ea4
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-12 08:55:15 +0530

    D_AM_JavaSourceFileStructure

---
+++ 0086 - D_AM_JavaSourceFileStructure.java
@@ -0,0 +1,16 @@
+package com.chandrika;
+
+public class D_AM_JavaSourceFileStructure
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hi");
+    }
+}
+/* public **/ class a
+{
+}
+
+class b
+{
+}
