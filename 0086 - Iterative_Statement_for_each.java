e97ac7ecac5204a96cf8163db59309ea4508e0a7
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-14 00:21:00 +0530

    Iterative_Statement_for_each.java

---
+++ 0086 - Iterative_Statement_for_each.java
@@ -0,0 +1,24 @@
+package com.chandrika;
+
+public class Iterative_Statement_for_each
+{
+    public static void main(String[] args)
+    {
+        int[] x = {10, 20, 30, 40, 50};
+
+        // Noemal loop
+
+//        System.out.println(x.length);
+//        for (int i = 0; i < x.length; i++)
+//        {
+//            System.out.println("The length of x is : " + x[i]);
+//        }
+
+        // for-each loop
+
+        for (int x1 : x)
+        {
+            System.out.println(x1);
+        }
+    }
+}
