053e9ab023b2dd8b961dcf1c67a42f40e71324ce
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-14 00:22:17 +0530

    Iterative_Statement

---
+++ 0087 - Iterative_Statement_for_each_1_Jpoint.java
@@ -0,0 +1,13 @@
+//  Traversing the array elements...
+package com.chandrika;
+
+public class Iterative_Statement_for_each_1_Jpoint
+{
+    public static void main(String[] args) {
+        int arr[] = {10, 47, 89, 35, 92};
+        for (int i : arr)
+        {
+            System.out.println(i);
+        }                
+    }
+}
