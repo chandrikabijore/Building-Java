8cf5f308f138a75b18f6a41d56f17c758ed856d7
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-14 22:29:56 +0530

    Iterative_Statement

---
+++ 0091 - Iterative_Statement_continue.java
@@ -0,0 +1,19 @@
+package com.chandrika;
+
+public class Iterative_Statement_continue
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if(i % 2 == 0)
+                continue;
+            System.out.println(i);
+        }
+
+        int x = 10;
+        if (x == 10);
+            // continue;
+        System.out.println("Hello");
+    }
+}
