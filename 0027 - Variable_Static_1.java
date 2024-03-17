fe78a6b19824303ae05ed96aff559a0ed5be8be4
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-17 20:55:14 +0530

    Variable

---
+++ 0027 - Variable_Static_1.java
@@ -0,0 +1,12 @@
+package com.chandrika;
+
+public class print_Statement
+{
+    //static String s = "Shivi";
+    public static void main(String[] args)
+    {
+        System.out.println(s);
+    }
+}
+
+
