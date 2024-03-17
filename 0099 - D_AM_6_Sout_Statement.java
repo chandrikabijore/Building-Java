e50cd6220e2892a00bdd0aee806491ae2fd9c331
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-17 21:08:55 +0530

    Declarations and Access Modifiers

---
+++ 0099 - D_AM_6_Sout_Statement.java
@@ -0,0 +1,17 @@
+package com.chandrika;
+// import java.io.PrintStream;
+import static java.lang.System.out;
+
+public class D_AM_6_Sout_Statement
+{
+    public static void main(String[] args)
+    {
+        out.println("Hello");
+        out.println("Hiiii");
+        System.out.println();
+    }
+}
+//class System
+//{
+//    static PrintStream out;
+//}
