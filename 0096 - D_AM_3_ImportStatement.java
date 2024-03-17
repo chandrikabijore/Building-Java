7160b02c703368262532dd65c15c895297725812
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-17 21:06:10 +0530

    Declarations and Access Modifiers

---
+++ 0096 - D_AM_3_ImportStatement.java
@@ -0,0 +1,14 @@
+package com.chandrika;
+
+import java.util.ArrayList;                  // import statement access typing shortcut
+
+public class D_AM_3_ImportStatement
+{
+    public static void main(String[] args)
+    {
+        ArrayList l = new ArrayList();
+        // System.out.println(ArrarList);               
+    }
+}
+
+
