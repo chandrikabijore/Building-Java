84c33d2a17e07e936659232c4e9460e4629d923f
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-17 21:10:47 +0530

    Declarations and Access Modifiers

---
+++ 0100 - D_AM_7_Sout_Statement.java
@@ -0,0 +1,11 @@
+package com.chandrika;
+// import static java.lang.Integer.MAX_VALUE;      // 2nd
+// import static java.lang.Byte.*;                 // 3rd
+public class D_AM_7_Sout_Statement
+{
+    static int MAX_VALUE = 999;                    // 1st
+    public static void main(String[] args)
+    {
+        System.out.println(MAX_VALUE);
+    }
+}
