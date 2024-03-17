919bf18fd0d63e308b8dd0de25ab3a2a0e03ea6a
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-17 21:07:05 +0530

    Declarations and Access Modifiers

---
+++ 0097 - D_AM_4_ImportStatement.java
@@ -0,0 +1,24 @@
+package com.chandrika;
+
+import java.util.Date;
+//import java.Sql.*;
+
+// 1)
+//class D_AM_ImportStatement_1 Myobject extends java.rmi.UnicastRemoteObject
+//{
+//}
+
+public class D_AM_4_ImportStatement
+{
+    public static void main(String[] args)
+    {
+        //2)
+        Date d = new Date();
+        System.out.println(d.getClass().getName());
+
+        // 3)
+//        String S = new String("Shivi");
+//        Student s1 = new Student("Shivi", 18);
+//        System.out.println(s1.name+"..."+s1.rollno);
+    }
+}
