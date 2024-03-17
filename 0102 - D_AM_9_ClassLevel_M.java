540288c6313528837e468254e8f2e9333a8cd943
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-17 21:12:37 +0530

    Declarations and Access Modifiers

---
+++ 0102 - D_AM_9_ClassLevel_M.java
@@ -0,0 +1,19 @@
+package com.chandrika;
+
+//private class A
+//{
+//    System.out.println("Shivi");
+//}
+//static class B
+//{
+//    System.out.println("Shivi");
+//}
+
+public class D_AM_9_ClassLevel_M
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Shivi");
+    }
+}
+
