41e1f7e8b812d736e5dfc2f9553d96a9d58163f4
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-17 21:13:38 +0530

    Declarations and Access Modifiers

---
+++ 0103 - D_AM_10_PublicClasses.java
@@ -0,0 +1,11 @@
+package com.chandrika;
+class D_AM_10_PublicClasses
+{
+    public static void main(String[] args) 
+    {
+       // public void m1()
+        {
+            System.out.println("Shivi Chandrika");
+        }
+    }
+}
