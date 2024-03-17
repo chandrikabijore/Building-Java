bda722a8062e10d97ae50c8c2da2d47e04f0d9d7
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-17 21:11:47 +0530

    Declarations and Access Modifiers

---
+++ 0101 - D_AM_8_Package.java
@@ -0,0 +1,11 @@
+// import java.util.*;
+package com.chandrika;
+
+public class D_AM_8_Package
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Demo package");
+    }
+}
+
