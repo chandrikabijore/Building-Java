838bbbc14eaae8201a745841e3a44f0b68ca80aa
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-22 20:55:06 +0530

    Declarations and Access Modifiers

---
+++ 0104 - D_AM_11_ClassLevel_M.java
@@ -0,0 +1,34 @@
+package com.chandrika;
+
+public class D_AM_11_ClassLevel_M
+{
+//    public static void main(String[] args)
+//    {
+//
+//    }
+}
+class p {
+    public void property() {
+        System.out.println("Cash + Land + Gold");
+    }
+
+    public void marry() {
+        System.out.println("Shubhlaxmi");
+    }
+}
+class c extends D_AM_11_ClassLevel_M
+{
+    public void marry()
+    {
+        System.out.println("Suraj, Yash, u me");
+    }
+}
+final class f
+{
+    static int x = 10;
+    public static void main(String[] args) {
+        x = 777;
+        System.out.println(x);
+    }
+}
+
