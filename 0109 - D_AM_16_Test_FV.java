bdca778139236da04ac3cff21495184638b20ecf
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-22 21:05:39 +0530

    Create 0109 - D_AM_16_Test_FV.java

---
+++ 0109 - D_AM_16_Test_FV.java
@@ -0,0 +1,12 @@
+package com.chandrika;
+
+public class D_AM_16_Test_FV
+{
+    static int y = 20;
+    static int x = 10;
+    public static void main(String[] args)
+    {
+        int z = 30;
+        System.out.println(x + " " + y + " " + z);
+    }
+}
