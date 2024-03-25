782344518293872a138d8a32a0fb4b5916017112
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-25 08:12:16 +0530

    Declarations and Access Modifiers

--- 0110 - D_AM_17_Formal_Parameters.java
+++ 0110 - D_AM_17_Formal_Parameters.java
@@ -4,12 +4,14 @@ public class D_AM_17_Formal_Parameters
 {
     public static void main(String[] args)
     {
-        m1(10, 20);
+        m1('s', 20);     // calling m1 with actual parameters
     }
-    public static void m1(final int x, int y)
+    public static void m1(final char x, int y)
     {
 //        x = 100;                                  // Formal parameter simply access LV of that method
 //        y = 200;                                  // But if Formal Parameter is declared as final then we can't perform reassignments
         System.out.println(x+".........."+y);
     }
 }
+
+ 
